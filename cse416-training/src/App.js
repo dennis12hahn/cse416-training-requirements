d763ec0634cb4a40fb6a9957c3c0b71e98118d66
dennis12hahn <dennis12hahn@gmail.com>
2021-10-05 10:26:11 -0400

    editable table

--- cse416-training/src/App.js
+++ cse416-training/src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { useTable } from 'react-table';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import { useTable } from "react-table";
+import "bootstrap/dist/css/bootstrap.css";
 
-import firebase from 'firebase/compat/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/compat/app";
+import "firebase/firestore";
+import "firebase/auth";
 
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useColelctionData } from 'react-firebase-hooks/firestore';
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useColelctionData } from "react-firebase-hooks/firestore";
 
 firebase.initializeApp({
   apiKey: "AIzaSyAs4XIS-dzpP36B95QQa4z0bk_PR8S53lk",
@@ -16,74 +16,117 @@ firebase.initializeApp({
   storageBucket: "cse416-training.appspot.com",
   messagingSenderId: "838408399543",
   appId: "1:838408399543:web:6c11f4dc6956fed2757b32",
-  measurementId: "G-E7F66BCT9L"
+  measurementId: "G-E7F66BCT9L",
 });
 
-function Table({ columns, data }) {
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = useTable({ columns, data })
+const EditableCell = ({
+  value: initialValue,
+  row: { index },
+  column: { id },
+  updateMyData,
+}) => {
+  const [value, setValue] = React.useState(initialValue);
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const onBlur = () => {
+    updateMyData(index, id, value);
+  };
+
+  React.useEffect(() => {
+    setValue(initialValue);
+  }, [initialValue]);
+
+  return <input value={value} onChange={onChange} onBlur={onBlur} />
+};
+
+const defaultColumn = {
+  Cell: EditableCell
+}
+
+function Table({ columns, data, updateMyData, skipPageReset }) {
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data, defaultColumn, autoResetPage: !skipPageReset, updateMyData });
 
   return (
-    <table class="table table-striped table-hover table-sm border table-responsive-sm mx-auto my-5 w-50"
-      {...getTableProps()} > 
+    <table
+      class="table table-striped table-hover table-sm border table-responsive-sm mx-auto my-5 w-50"
+      {...getTableProps()}
+    >
       <thead>
-        {headerGroups.map(headerGroup => (
+        {headerGroups.map((headerGroup) => (
           <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
             ))}
           </tr>
         ))}
       </thead>
       <tbody {...getTableBodyProps()}>
         {rows.map((row, i) => {
-          prepareRow(row)
+          prepareRow(row);
           return (
             <tr {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+              {row.cells.map((cell) => {
+                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
               })}
             </tr>
-          )
+          );
         })}
       </tbody>
     </table>
-  )
+  );
 }
 
 function App() {
-
   const columns = React.useMemo(
     () => [
       {
-        Header: 'First Name',
-        accessor: 'first_name',
+        Header: "First Name",
+        accessor: "first_name",
       },
       {
-        Header: 'Last Name',
-        accessor: 'last_name',
+        Header: "Last Name",
+        accessor: "last_name",
       },
       {
-        Header: 'Email',
-        accessor: 'email',
+        Header: "Email",
+        accessor: "email",
       },
       {
-        Header: 'Gender',
-        accessor: 'gender',
-      }],
+        Header: "Gender",
+        accessor: "gender",
+      },
+    ],
     []
-  )
-
-  const data = React.useMemo(() => require('./MOCK_DATA.json'), [])
-
-  return (
-    <Table columns={columns} data={data} />
   );
+
+  const [data, setData] = React.useState(() => require("./MOCK_DATA.json"), []);
+  const [originalData] = React.useState(data)
+  const [skipPageReset, setSkipPageReset] = React.useState(false)
+
+  const updateMyData = (rowIndex, columnId, value) => {
+    setSkipPageReset(true);
+    setData((old) =>
+      old.map((row, index) => {
+        if (index === rowIndex) {
+          return {
+            ...old[rowIndex],
+            [columnId]: value,
+          };
+        }
+        return row;
+      })
+    );
+  };
+
+  React.useEffect(() => {
+    setSkipPageReset(false);
+  }, [data])
+
+  return <Table columns={columns} data={data} updateMyData={updateMyData} />;
 }
 
 export default App;
